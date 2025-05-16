9861444253d4e5d3a9eab10a4890998e1ec8331c
artorias305 <caique.andraus@gmail.com>
2025-05-15 21:09:17 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+// import "./App.css";
+import { useEffect, useState } from "react";
+import "./style.css";
+
+function App() {
+  const [cards, setCards] = useState([
+    "mrekk",
+    "Ivaxa",
+    "NINERIK",
+    "Accolibed",
+    "gnahus",
+    "milosz",
+    "NyanPotato",
+    "lifeline",
+    "bored yes",
+    "JappaDeKappa",
+  ]);
+
+  const [score, setScore] = useState(0);
+
+  const [bestScore, setBestScore] = useState(0);
+
+  const [clickedCards, setClickedCards] = useState([]);
+
+  const shuffle = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  useEffect(() => {
+    console.log("Clicked cards updated:", clickedCards);
+  }, [clickedCards]);
+
+  const handleClick = (card) => {
+    if (clickedCards.includes(card)) {
+      if (score > bestScore) {
+        setBestScore(score);
+      }
+      setScore(0);
+      setClickedCards([]);
+    } else {
+      setClickedCards([...clickedCards, card]);
+      setScore(score + 1);
+    }
+    setCards(shuffle(cards));
+  };
+
+  return (
+    <div>
+      <div>
+        <header>
+          <p>Score: {score}</p>
+          <p>Best Score: {bestScore}</p>
+        </header>
+      </div>
+      <div id="cards">
+        <ul>
+          {cards.map((card) => (
+            <li key={card}>
+              <button onClick={() => handleClick(card)}>{card}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
